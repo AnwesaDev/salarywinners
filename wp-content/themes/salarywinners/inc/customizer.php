9e68d2c3be77cbc9987213cf4bfe27ca102db77a
anwesa <anwesa@192.168.1.104>
2016-04-28 12:31:35 +0530

    Theme added

---
+++ wp-content/themes/salarywinners/inc/customizer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Salary Winners Theme Customizer.
+ *
+ * @package Salary_Winners
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function salarywinners_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+}
+add_action( 'customize_register', 'salarywinners_customize_register' );
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function salarywinners_customize_preview_js() {
+	wp_enqueue_script( 'salarywinners_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'salarywinners_customize_preview_js' );
