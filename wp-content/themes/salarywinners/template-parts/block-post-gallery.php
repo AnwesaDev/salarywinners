b898718efa826250c25ced6984fc9112d82da65a
Sandeep Guria <hey@sandeepguria.in>
2016-04-29 12:20:26 +0530

    blog gallery

--- wp-content/themes/salarywinners/template-parts/block-post-gallery.php
+++ wp-content/themes/salarywinners/template-parts/block-post-gallery.php
@@ -5,60 +5,32 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+$args = array(
+    'post_type' => 'post',
+    'posts_per_page' => 4
+);
+
+$queryPostGallery = new WP_Query($args);
 ?>
     <section class="gallery">
           <div class="row">
+              <?php if($queryPostGallery->have_posts()): ?>
+              <?php while($queryPostGallery->have_posts()): $queryPostGallery->the_post(); ?>
               <div class="gallery-box">
                   <div class="thumb">
-                  	<img src="<?php echo get_template_directory_uri();?>/images/img-thumb1.jpg" alt="" title="">
+<!--                  	<img src="<?php echo get_template_directory_uri();?>/images/img-thumb1.jpg" alt="" title="">-->
+                      <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a>
                   </div>
                   <div class="thumb-content">
-                  	<h3 class="title">Hi, this is title</h3>
-                    <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam lacus neque.
-                    </p>
+                      <h3 class="title"><?php the_title(); ?></h3>
+                    
+                    <?php the_excerpt(); ?>
+                   
                     <i></i>
                   </div>
               </div>
+              <?php endwhile; ?>
+              <?php endif; ?>
               
-              <div class="gallery-box">
-                  <div class="thumb">
-                  	<img src="<?php echo get_template_directory_uri();?>/images/img-thumb2.jpg" alt="" title="">
-                  </div>
-                  <div class="thumb-content">
-                  	<h3 class="title">Hi, this is title</h3>
-                    <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam lacus neque.
-                    </p>
-                    <i></i>
-                  </div>
-              </div>
-              
-              <div class="gallery-box">
-                  <div class="thumb-content">
-                  	<h3 class="title">Hi, this is title</h3>
-                    <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam lacus neque.
-                    </p>
-                    <i></i>
-                  </div>
-                   <div class="thumb">
-                  	<img src="<?php echo get_template_directory_uri();?>/images/img-thumb3.jpg" alt="" title="">
-                  </div>
-              </div>
-              
-              <div class="gallery-box">
-                  <div class="thumb-content">
-                  	<h3 class="title">Hi, this is title</h3>
-                    <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam lacus neque.
-                    </p>
-                    <i></i>
-                  </div>
-                   <div class="thumb">
-                  	<img src="<?php echo get_template_directory_uri();?>/images/img-thumb4.jpg" alt="" title="">
-                  </div>
-              </div>
-            
            </div>
         </section>
