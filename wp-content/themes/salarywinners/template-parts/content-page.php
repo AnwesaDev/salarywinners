65a4f16223b001af4c65fb533a59690b74fda2f0
suman.anwesa <suman.anwesa@gmail.com>
2016-05-06 13:05:10 +0530

    update

--- wp-content/themes/salarywinners/template-parts/content-page.php
+++ wp-content/themes/salarywinners/template-parts/content-page.php
@@ -14,7 +14,7 @@
         	<div class="container">
             	<div class="row">
                 	<!--Main Conatent section-->
-                	<div class="col-md-8 col-sm-8">
+                	<div class="col-md-9 col-sm-8">
                             
                         <div class="content">
                             <h2 class="title"><?php the_title();?></h2>
@@ -23,15 +23,17 @@
                     </div>
                     
                     <!--Side Bar-->
-                    <div class="col-md-4 col-sm-4">
-                    	<div class="sidebar">
-                        	<ul>
-                            	<li><span class="fa fa-question"></span>FAQ</li>
-                                <li><span class="fa fa-mobile-phone"></span>02 3000 1234</li>
-                                <li><span class="fa fa-envelope"></span>hello@asallerywiners.com</li>
-                                <li><span class="fa fa-headphones"></span>Online chat and ask us to call you</li>
-                            </ul>
-                        </div>
+                    <div class="col-md-3 col-sm-4">
+                    	<div class="row">
+							<div class="sidebar">
+								<ul>
+									<li><span class="fa fa-question"></span>FAQ</li>
+									<li><span class="fa fa-mobile-phone"></span>02 3000 1234</li>
+									<li><span class="fa fa-envelope"></span>hello@asallerywiners.com</li>
+									<li><span class="fa fa-headphones"></span>Online chat and ask us to call you</li>
+								</ul>
+							</div>
+						</div>
                     </div>
                 </div>
             </div>
